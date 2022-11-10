d46ce7161b576ff781e69044ebe686fe6d439b85
Andrés Navarrete <ainavarrete@uc.cl>
2022-11-10 12:55:00 -0300

    Add join on teams and result 2021

---
+++ src/club_names.py
@@ -0,0 +1,23 @@
+
+name_dict = {
+    'Arsenal': 'Arsenal',
+    'Leeds': 'Leeds United',
+    'Brighton': 'Brighton & Hove Albion',
+    'Crystal Palace': 'Crystal Palace',
+    'Southampton': 'Southampton',
+    'Wolves': 'Wolverhampton Wanderers',
+    'Aston Villa': 'Aston Villa',
+    'Liverpool': 'Liverpool',
+    'West Ham': 'West Ham United',
+    'Man City': 'Manchester City',
+    'Burnley': 'Burnley',
+    'Newcastle': 'Newcastle United',
+    'Everton': 'Everton',
+    'Man United': 'Manchester United',
+    'Tottenham': 'Tottenham Hotspur',
+    'Chelsea': 'Chelsea',
+    'Leicester': 'Leicester City',
+    'West Brom': 'West Bromwich Albion',
+    'Fulham': 'Fulham',
+    'Sheffield United': 'Sheffield United'
+}
