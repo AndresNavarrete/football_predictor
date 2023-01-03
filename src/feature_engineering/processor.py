37eb9e0c25d0e86cf7d34d97bc30162a90404c8c
Andrés Navarrete <ainavarrete@uc.cl>
2023-01-02 22:00:46 -0300

    add fixture

---
+++ src/feature_engineering/processor.py
@@ -0,0 +1,132 @@
+import pandas as pd 
+from datetime import datetime 
+import duckdb 
+
+class Processor:
+    
+    def __init__(self):
+        self.PREMIER_PATH = "data/results_premier/results.csv"
+        self.ML_PATH = "data/ml"
+        self.con = duckdb.connect()
+        self.records = list()
+        self.df = None
+        self.useful_columns = [
+            "season",
+            "team",
+            "opponent",
+            "datetime",
+            "veneau",
+            "GF",
+            "GA",
+            "result",
+            ]
+    
+    def execute(self):
+        self.load_raw_data()
+        self.serialize_data()
+        self.add_streak_columns()
+        self.add_categorical_columns()
+        self.clean_data()
+        self.export_data()
+    
+    def load_raw_data(self):
+        self.df = pd.read_csv(
+            self.PREMIER_PATH, 
+            encoding = "ISO-8859-1"
+            )
+    
+    def serialize_data(self):
+        self.df.apply(lambda row: self.add_records_from_row(row), axis=1)
+        self.df = pd.DataFrame(self.records)[self.useful_columns]
+
+    def add_records_from_row(self, row):
+        home = self.get_home_record(row)
+        away  = self.get_away_record(row)
+        self.records.extend([home,away])
+    
+    def get_home_record(self, row):
+        return {
+        'season': row.Season,
+        'team': row.HomeTeam,
+        'opponent': row.AwayTeam,
+        'datetime': self.get_datetime(row.DateTime),
+        'veneau': 'home',
+        'GF': row.FTHG,
+        'GA': row.FTAG,
+        'result': self.get_match_result(row.FTHG, row.FTAG),
+    }
+
+    def get_datetime(self, date_raw):
+        date_str = date_raw.strip('Z').replace('T',' ')
+        return datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
+
+    def get_match_result(self, GF, GA):
+        if GF == GA:
+            return 'D'
+        if GA > GF:
+            return 'L'
+        else:
+            return 'W'
+
+    def get_away_record(self, row):
+        return {
+            'season': row.Season,
+            'team': row.AwayTeam,
+            'opponent': row.HomeTeam,
+            'datetime': self.get_datetime(row.DateTime),
+            'veneau': 'away',
+            'GF': row.FTAG,
+            'GA': row.FTHG,
+            'result': self.get_match_result(row.FTAG, row.FTHG),
+        } 
+
+    def add_streak_columns(self):
+        df_matches = self.df
+        query = """
+            WITH matches AS (
+            SELECT
+                *,
+                ROW_NUMBER() OVER (PARTITION BY season, team ORDER BY datetime) AS match_number
+            FROM df_matches
+            ORDER BY season, datetime)
+            SELECT
+                m.*,
+                (SELECT COUNT(*) FROM matches WHERE season = m.season AND team = m.team AND result = 'W' AND match_number > m.match_number - 6 AND match_number < m.match_number) AS last_wins,
+                (SELECT COUNT(*) FROM matches WHERE season = m.season AND team = m.team AND result = 'L' AND match_number > m.match_number - 6 AND match_number < m.match_number) AS last_loses,
+                (SELECT COUNT(*) FROM matches WHERE season = m.season AND team = m.team AND result = 'D' AND match_number > m.match_number - 6 AND match_number < m.match_number) AS last_draws
+            FROM matches m
+            ORDER BY season, datetime;
+        """
+        self.df = self.con.execute(query).df()
+    
+    def add_categorical_columns(self):
+        self.df["home_code"] = self.df.team.astype("category").cat.codes
+        self.df["away_code"] = self.df.opponent.astype("category").cat.codes
+    
+    def clean_data(self):
+        df_matches = self.df
+        query = """
+            select 
+                team as home,
+                opponent as away,
+                home_code,
+                away_code,
+                last_wins,
+                last_draws,
+                last_loses,
+                GF,
+                GA
+            from df_matches
+            where 
+                GF < 10 and 
+                GA < 10
+            """
+        self.df = self.con.execute(query).df()
+    
+    def export_data(self):
+        self.df.to_csv(f'{self.ML_PATH}/df_history.csv', index=False)
+
+
+if __name__ == "__main__":
+    processor = Processor()
+    processor.execute()
---
+++ src/providers/fixture.py
@@ -0,0 +1,55 @@
+import requests
+import json
+import pandas as pd
+import duckdb
+
+class Fixture:
+    def __init__(self):
+        self.url = "https://fixturedownload.com/feed/json/epl-2022"
+        self.con = duckdb.connect()
+        self.fixture = None
+        self.names_translator = {
+            'Man Utd': 'Man United',
+            'Spurs': 'Tottenham',
+            'Nottingham Forest': """Nott'm Forest"""
+        }
+    
+    def get_fixture(self):
+        raw_data = self.download_data()
+        self.serialize_data(raw_data)
+        self.fix_club_names()
+        return self.fixture
+    
+    def download_data(self):
+        response = requests.request("GET", self.url)
+        return pd.DataFrame(json.loads(response.text)).fillna("")
+
+    def serialize_data(self, raw_data):
+        query = """
+        SELECT
+            MatchNumber::INT AS id,
+            RoundNumber::INT AS round,
+            DateUtc::DATE AS DATE,
+            HomeTeam AS home,
+            AwayTeam AS away,
+            TRY_CAST(HomeTeamScore as INTEGER)  as home_goals,
+            TRY_CAST(AwayTeamScore as INTEGER)  as away_goals,
+            CASE
+                WHEN DateUtc::DATE > now() THEN FALSE ELSE TRUE 
+            END AS played 
+        FROM
+            raw_data        
+        """
+        self.fixture = self.con.execute(query).df()
+    
+    def fix_club_names(self):
+        replacement =  {
+            "home": self.names_translator,
+            "away":self.names_translator,
+        }
+        return self.fixture.replace(replacement)
+
+if __name__ == "__main__":
+    provider = Fixture()
+    fixture = provider.get_fixture()
+    print(fixture)
